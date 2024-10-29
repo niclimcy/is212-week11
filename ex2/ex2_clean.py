0a9e1d07c6832e83da29486e8eb0892292291e05
Nicholas Lim <nicknitewolf@gmail.com>
2024-10-29 11:13:15 +0800

    ex2: Refactor code

---
+++ ex2/ex2_clean.py
@@ -0,0 +1,29 @@
+class Shape:
+    def calculate_area():
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return 3.14 * self.radius * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
